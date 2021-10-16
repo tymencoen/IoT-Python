26a4225d7fa9488c5ad02b032584a4c901146986
tymencoen <91782904+tymencoen@users.noreply.github.com>
2021-10-16 16:21:55 +0200

    Add files via upload

---
+++ foutzoeken.py
@@ -0,0 +1,7 @@
+print("Misschien vind je hier al de eerste fout?")
+a="10"
+b="20"
+print(((a)*5))
+print((a+b)/3)
+hex("25")
+print("Als je dit ziet in de output ben je op het einde van de opdracht geraakt. Proficiat!")
